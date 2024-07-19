eaa602f930e80573af8a95a64075f1a286b4fb88
osalyousef <os.forwork1@gmail.com>
2024-07-19 07:38:32 +0300

    Initial Commit

---
+++ mdaqash/mdaqashApp.swift
@@ -0,0 +1,17 @@
+//
+//  mdaqashApp.swift
+//  mdaqash
+//
+//  Created by Osamah alyousef on 19/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct mdaqashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
